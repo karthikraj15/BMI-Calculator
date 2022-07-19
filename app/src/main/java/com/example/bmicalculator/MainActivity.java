a7b564c07eaca523c17374e7df709a86014b48c6
Karthik raj R <karthikraj158r@gmail.com>
2022-07-19 20:08:57 +0530

    first commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,257 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mcurrentweight,mcurrentage,mcurrentheight;
+
+    ImageView mincrementage,mdecrementage,mincrementweight,mdecrementweight;
+    SeekBar mseekbarforheight;
+    Button mcalculatebmi;
+    RelativeLayout mmale,mfemale;
+
+    int intweight=55;
+    int intage=22;
+    int currentprogress;
+    String mintprogress="170";
+    String typerofuser="0";
+    String weight2="55";
+    String age2="22";
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        mcurrentage=findViewById(R.id.currentage);
+        mcurrentweight=findViewById(R.id.currentweight);
+        mcurrentheight=findViewById(R.id.currentheight);
+        mincrementage=findViewById(R.id.incrementage);
+        mdecrementage=findViewById(R.id.decrementage);
+        mincrementweight=findViewById(R.id.incremetweight);
+        mdecrementweight=findViewById(R.id.decrementweight);
+        mcalculatebmi=findViewById(R.id.calculatebmi);
+        mseekbarforheight=findViewById(R.id.seekbarforheight);
+        mmale=findViewById(R.id.male);
+        mfemale=findViewById(R.id.female);
+
+
+
+        mmale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        mmale.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.malefemalefocus));
+                        mfemale.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.malefemalenotfocus));
+                        typerofuser="Male";
+
+            }
+        });
+
+
+      mfemale.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              mfemale.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.malefemalefocus));
+              mmale.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.malefemalenotfocus));
+              typerofuser="Female";
+          }
+      });
+
+        mseekbarforheight.setMax(300);
+        mseekbarforheight.setProgress(170);
+        mseekbarforheight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                currentprogress=progress;
+                mintprogress=String.valueOf(currentprogress);
+                mcurrentheight.setText(mintprogress);
+                mcurrentheight.setSelection(mintprogress.length());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        mcurrentheight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String data=mcurrentheight.getText().toString();
+                if(data.length()==0)
+                    mintprogress="0";
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                try{
+                    int progress=Integer.parseInt(editable.toString());
+                    mseekbarforheight.setProgress(progress);
+                    mintprogress=String.valueOf(progress);
+                }catch (Exception e){}
+            }
+        });
+
+
+        mcurrentage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String data=mcurrentage.getText().toString();
+                if(data.length()==0)
+                    intage=0;
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                try{
+                    int p1=Integer.parseInt(editable.toString());
+                    age2=String.valueOf(p1);
+                    intage=p1;
+
+                }catch (Exception e){}
+            }
+        });
+
+
+        mcurrentweight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String data=mcurrentweight.getText().toString();
+                if(data.length()==0)
+                    intweight=0;
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                try{
+                    int p2=Integer.parseInt(editable.toString());
+                    intweight=p2;
+                    weight2=String.valueOf(p2);
+
+
+                }catch (Exception e){}
+            }
+        });
+        mincrementweight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intweight=intweight+1;
+                weight2=String.valueOf(intweight);
+                mcurrentweight.setText(weight2);
+            }
+        });
+
+        mincrementage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intage=intage+1;
+                age2=String.valueOf(intage);
+                mcurrentage.setText(age2);
+            }
+        });
+
+
+        mdecrementage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intage=intage-1;
+                age2=String.valueOf(intage);
+                mcurrentage.setText(age2);
+            }
+        });
+
+
+        mdecrementweight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intweight=intweight-1;
+                 weight2=String.valueOf(intweight);
+                mcurrentweight.setText(weight2);
+            }
+        });
+
+
+
+        mcalculatebmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(typerofuser.equals("0"))
+                {
+                    Toast.makeText(getApplicationContext(),"Select Your Gender First",Toast.LENGTH_SHORT).show();
+                }
+                else if(mintprogress.equals("0") || mintprogress.equals("") )
+                {
+                    Toast.makeText(getApplicationContext(),"Select Your Height First",Toast.LENGTH_SHORT).show();
+                }
+                else if(intage==0 || intage<0 || age2.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(),"Age is Incorrect",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(intweight==0|| intweight<0 || weight2.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(),"Weight Is Incorrect",Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    Intent intent = new Intent(MainActivity.this, bmiactivity.class);
+                    intent.putExtra("gender", typerofuser);
+                    intent.putExtra("height", mintprogress);
+                    intent.putExtra("weight", weight2);
+                    intent.putExtra("age", age2);
+                    startActivity(intent);
+
+                }
+
+
+            }
+        });
+
+
+    }
+}
