745a4088aeeb12c18136d5abe830ac4117e97cfe
Karthik raj R <88207381+karthikraj15@users.noreply.github.com>
2022-07-25 22:36:02 +0530

    Updated

--- app/src/main/java/com/example/bmicalculator/bmiactivity.java
+++ app/src/main/java/com/example/bmicalculator/bmiactivity.java
@@ -16,15 +16,13 @@ import android.widget.TextView;
 
 public class bmiactivity extends AppCompatActivity {
 
-
-
-    TextView mbmidisplay,magedisplay,mweightdisplay,mheightdisplay,mbmicategory,mgender;
+    TextView mbmidisplay,mbmicategory,mgender;
     Button mgotomain;
     Intent intent;
 
     ImageView mimageview;
     String mbmi;
-    String cateogory;
+
     float intbmi;
 
     String height;
@@ -43,31 +41,22 @@ public class bmiactivity extends AppCompatActivity {
         ColorDrawable colorDrawable=new ColorDrawable(Color.parseColor("#1E1D1D"));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
 
-     ///   ColorDrawable colorDrawable2=new ColorDrawable(Color.parseColor("#1E1D1D"));
-  //      getSupportActionBar().setBackgroundDrawable(colorDrawable);
-
-
         getSupportActionBar().setTitle(Html.fromHtml("<font color=\"white\"></font>"));
         getSupportActionBar().setTitle("Result");
 
-
         intent=getIntent();
         mbmidisplay=findViewById(R.id.bmidisplay);
-    //    magedisplay=findViewById(R.id.agedisplay);
-    //    mweightdisplay=findViewById(R.id.weightdisplay);
+
         mbmicategory = findViewById(R.id.bmicategorydispaly);
         mgotomain=findViewById(R.id.gotomain);
 
         mimageview=findViewById(R.id.imageview);
 
-     //   mheightdisplay=findViewById(R.id.heightdisplay);
         mgender=findViewById(R.id.genderdisplay);
         mbackground=findViewById(R.id.contentlayout);
 
-
         height=intent.getStringExtra("height");
         weight=intent.getStringExtra("weight");
-
 
         intheight=Float.parseFloat(height);
         intweight=Float.parseFloat(weight);
@@ -75,17 +64,14 @@ public class bmiactivity extends AppCompatActivity {
         intheight=intheight/100;
         intbmi=intweight/(intheight*intheight);
 
-
         mbmi=Float.toString(intbmi);
         System.out.println(mbmi);
 
         if(intbmi<16)
         {
             mbmicategory.setText("Severe Thinness");
-        //   mbackground.setBackgroundColor(Color.GRAY);
             mbackground.setBackgroundColor(Color.RED);
             mimageview.setImageResource(R.drawable.crosss);
-          //  mimageview.setBackground(colorDrawable2);
 
         }
         else if(intbmi<16.9 && intbmi>16)
@@ -93,7 +79,6 @@ public class bmiactivity extends AppCompatActivity {
             mbmicategory.setText("Moderate Thinness");
             mbackground.setBackgroundColor(R.color.halfwarn);
             mimageview.setImageResource(R.drawable.warning);
-         //   mimageview.setBackground(colorDrawable2);
 
         }
         else if(intbmi<18.4 && intbmi>17)
@@ -101,43 +86,38 @@ public class bmiactivity extends AppCompatActivity {
             mbmicategory.setText("Mild Thinness");
             mbackground.setBackgroundColor(R.color.halfwarn);
             mimageview.setImageResource(R.drawable.warning);
-       //   mimageview.setBackground(colorDrawable2);
+
         }
         else if(intbmi<24.9 && intbmi>18.5 )
         {
             mbmicategory.setText("Normal");
             mimageview.setImageResource(R.drawable.ok);
-           // mbackground.setBackgroundColor(Color.YELLOW);
-          //  mimageview.setBackground(colorDrawable2);
+
         }
         else if(intbmi <29.9 && intbmi>25)
         {
             mbmicategory.setText("Overweight");
             mbackground.setBackgroundColor(R.color.halfwarn);
             mimageview.setImageResource(R.drawable.warning);
-            //mimageview.setBackground(colorDrawable2);
+
         }
         else if(intbmi<34.9 && intbmi>30)
         {
             mbmicategory.setText("Obese Class I");
             mbackground.setBackgroundColor(R.color.halfwarn);
             mimageview.setImageResource(R.drawable.warning);
-          //  mimageview.setBackground(colorDrawable2);
+
         }
         else
         {
             mbmicategory.setText("Obese Class II");
             mbackground.setBackgroundColor(R.color.warn);
             mimageview.setImageResource(R.drawable.crosss);
-          //  mimageview.setBackground(colorDrawable2);
+
         }
 
-        //magedisplay.setText("your age is"+intent.getStringExtra("age"));
-       //mheightdisplay.setText("Your Height is "+intent.getStringExtra("height"));
-        //mweightdisplay.setText("Your Weight is "+intent.getStringExtra("weight"));
         mgender.setText(intent.getStringExtra("gender"));
         mbmidisplay.setText(mbmi);
-
 
         mgotomain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,7 +126,5 @@ public class bmiactivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
-
-
     }
 }
